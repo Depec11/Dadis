e10c6d7905941483e21154e5aa8fa09d6320afe5
Depec o Djemi <ywb3295326723@outlook.com>
2024-11-24 09:37:16 +0800

    基础的架构，分为DayManager和NightManager两个弱场景

---
+++ Assets/Scripts/Frame/UsefulFunctions.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public static class UsefulFunctions {
+    public static void DestroyAllChildren(Transform parent) {
+        foreach (Transform t in parent) { 
+            GameObject.Destroy(t.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/MainScene.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using static UsefulFunctions;
+
+public class MainScene : MonoBehaviour {
+    [SerializeField] private GameObject m_dayScenes;
+    [SerializeField] private GameObject m_nightScenes;
+    private void Awake() {
+        
+    }
+    public void GenerateDayScene() {
+        DestroyAllChildren(transform);
+        Instantiate(m_dayScenes, transform);
+    }
+    public void GenerateNightScene() {
+        DestroyAllChildren(transform);
+        Instantiate (m_nightScenes, transform);
+    }
+}
