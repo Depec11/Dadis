523b20a36be433b476c1389c8dad98af74bb53eb
Depec o Djemi <ywb3295326723@outlook.com>
2024-11-24 14:25:59 +0800

    测试忽略.idea文件夹

--- Assets/Scripts/MainScene.cs
+++ Assets/Scripts/MainScene.cs
@@ -1,18 +1,37 @@
+// ENCODING WITH UTF-8
+
 using UnityEngine;
+using System.Threading.Tasks;
 using static UsefulFunctions;
 
 public class MainScene : MonoBehaviour {
+    /// <summary>
+    /// 白天场景
+    /// </summary>
     [SerializeField] private GameObject m_dayScenes;
+    /// <summary>
+    /// 夜晚场景
+    /// </summary>
     [SerializeField] private GameObject m_nightScenes;
     private void Awake() {
         GenerateNightScene();
     }
+    /// <summary>
+    /// 生成白天场景，会摧毁其他场景
+    /// </summary>
     public void GenerateDayScene() {
         DestroyAllChildren(transform);
         Instantiate(m_dayScenes, transform);
     }
+    /// <summary>
+    /// 生成夜晚场景，会摧毁其他场景
+    /// </summary>
     public void GenerateNightScene() {
         DestroyAllChildren(transform);
         Instantiate (m_nightScenes, transform);
+    }
+
+    public async Task PrepareSceneAsync() {
+        return;
     }
 }
