cef786137b524e17bda643e9eca8e8e3dcce6464
Depec o Djemi <ywb3295326723@outlook.com>
2024-11-24 11:05:06 +0800

    显示地图

--- Assets/Scripts/Game/Night/NightManager.cs
+++ Assets/Scripts/Game/Night/NightManager.cs
@@ -2,7 +2,19 @@ using System.Collections.Generic;
 using UnityEngine;
 public class NightManager : SceneManager {
     [SerializeField] private MapChanceSheet m_mapChanceSheet;
+    /// <summary>
+    ///  [i, j]，i是行，j是列
+    /// </summary>
     private NightMapStateEnum[,] m_map;
+    [SerializeField] private Transform m_mapTransform;
+    // private static readonly string CHEST_PATH = "Scenes/Night/Prefabs/Chest";
+    // private static readonly string PROP_PATH = "";
+    // private static readonly string PATH_PATH = "";
+    // private static readonly string MONSTER_PATH = "";
+    [SerializeField] private GameObject m_chestPrefab;
+    [SerializeField] private GameObject m_propPrefab;
+    [SerializeField] private GameObject m_pathPrefab;
+    [SerializeField] private GameObject m_monsterPrefab;
     protected override void Awake() {
         base.Awake();
         GenerateMap();
@@ -15,7 +27,25 @@ public class NightManager : SceneManager {
         for (int i = 0; i < m_mapChanceSheet.MapCount; i++) {
             for (int j = 0; j < m_mapChanceSheet.MapCount; j++) {
                 m_map[i, j] = m_mapChanceSheet.GetRandomValue(sample);
+                switch (m_map[i, j]) {
+                    case NightMapStateEnum.CHEST:
+                        InstantiateTile(m_chestPrefab, i, j);
+                        break;
+                    case NightMapStateEnum.PROP:
+                        InstantiateTile(m_propPrefab, i, j);
+                        break;
+                    case NightMapStateEnum.PATH:
+                        InstantiateTile(m_pathPrefab, i, j);
+                        break;
+                    case NightMapStateEnum.MONSTER:
+                        InstantiateTile(m_monsterPrefab, i, j);
+                        break;
+                }
             }
         }
+    }
+    private void InstantiateTile(GameObject go, int r, int c) {
+        GameObject instance = Instantiate(go, m_mapTransform);
+        instance.transform.position = new Vector2(c, -r);   // Unity 和 计算机图形学的坐标系不一样，:(
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - 分为白天和黑天两个弱场景（即为Prefabs）
 - 两个弱场景在生成后从StreamingAssets中加载Json数据初始化场景，或者使用Global
 - 有一个全局主场景，目前只能用于跳转弱场景
+## 切记
+- 不要使用Text Mesh Pro，UI就用Lengacy
