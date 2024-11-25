c4756b30b70edb42f79107db4ff49479e2d7c3d4
Depec o Djemi <ywb3295326723@outlook.com>
2024-11-25 09:28:20 +0800

    黑天玩家移动

--- Assets/Scripts/Night/NightManager.cs
+++ Assets/Scripts/Night/NightManager.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 using UnityEngine.Serialization;
 public class NightManager : Frame.SceneManager {
     public static NightManager Instance { get; private set; }
-    [FormerlySerializedAs("m_NightMapChanceSheet")] [FormerlySerializedAs("mNightMapChanceSheet")] [FormerlySerializedAs("m_mapChanceSheet")] [SerializeField] private NightMapChanceSheet m_nightMapChanceSheet;
+    [SerializeField] private NightMapChanceSheet m_nightMapChanceSheet;
     /// <summary>
     ///  [i, j]，i是行，j是列
     /// </summary>
     private NightMapStateEnum[,] m_map;
     private bool[,] m_shadow;
-    [SerializeField] private Transform m_mapTransform;
-    [SerializeField] private Transform m_shadowTransform;
+    [SerializeField] private Transform m_mapParent;
+    [SerializeField] private Transform m_shadowParent;
+    [SerializeField] private Transform m_playerParent;
     [SerializeField] private GameObject m_chestPrefab;
     [SerializeField] private GameObject m_propPrefab;
     [SerializeField] private GameObject m_pathPrefab;
     [SerializeField] private GameObject m_monsterPrefab;
     [SerializeField] private GameObject m_playerPrefab;
     [SerializeField] private GameObject m_shadowPrefab;
+    private List<Vector2Int> m_paths = new();
     public NightPlayer Player { get; private set; }
+    [SerializeField] private float m_playerSpeed = 10.0f;
     protected override void Awake() {
         base.Awake();
         Instance = this;
@@ -27,11 +30,14 @@ public class NightManager : Frame.SceneManager {
         throw new System.NotImplementedException();
     }
     private void Update() {
-        if (Input.GetMouseButtonUp(0)) {
+        if (m_paths.Count == 0 && Input.GetMouseButtonUp(0)) {
             HandleMove();
+        } else if (m_paths.Count > 0) {
+            Move();
         }
         if (Player) {
             Vector3 pos = Player.transform.position;
+            Player.transform.localPosition = new Vector3(pos.x, pos.y, 0);
             pos.z = Camera.main.transform.position.z;
             MainScene.MainCamera.transform.position = pos;
         }
@@ -45,21 +51,21 @@ public class NightManager : Frame.SceneManager {
         for (int i = 0; i < m_nightMapChanceSheet.MapCount; i++) {
             for (int j = 0; j < m_nightMapChanceSheet.MapCount; j++) {
                 m_shadow[i, j] = true;
-                InstantiateTile(m_shadowPrefab , i,j, m_shadowTransform);
+                InstantiateTile(m_shadowPrefab , i,j, m_shadowParent);
                 m_map[i, j] = m_nightMapChanceSheet.GetRandomValue(sample);
                 switch (m_map[i, j]) {
                     case NightMapStateEnum.CHEST:
-                        InstantiateTile(m_chestPrefab, i, j, m_mapTransform);
+                        InstantiateTile(m_chestPrefab, i, j, m_mapParent);
                         break;
                     case NightMapStateEnum.PROP:
-                        InstantiateTile(m_propPrefab, i, j, m_mapTransform);
+                        InstantiateTile(m_propPrefab, i, j, m_mapParent);
                         break;
                     case NightMapStateEnum.PATH:
                         paths.Add(new Vector2Int(i, j));
-                        InstantiateTile(m_pathPrefab, i, j, m_mapTransform);
+                        InstantiateTile(m_pathPrefab, i, j, m_mapParent);
                         break;
                     case NightMapStateEnum.MONSTER:
-                        InstantiateTile(m_monsterPrefab, i, j, m_mapTransform);
+                        InstantiateTile(m_monsterPrefab, i, j, m_mapParent);
                         break;
                 }
             }
@@ -76,7 +82,7 @@ public class NightManager : Frame.SceneManager {
         return instance;
     }
     private void InstantiatePlayer(Vector2Int pos) {
-        Player = InstantiateTile(m_playerPrefab, pos.x, pos.y, transform).GetComponent<NightPlayer>();
+        Player = InstantiateTile(m_playerPrefab, pos.x, pos.y, m_playerParent).GetComponent<NightPlayer>();
         Player.Position = pos;
         ShowMap(pos);
     }
@@ -89,16 +95,15 @@ public class NightManager : Frame.SceneManager {
         }
         if (m_shadow[index.x, index.y]) {
             // Debug.Log(string.Format("IS SHADOW {0}", index));
-            return;
+            if (!CanGoAsShadow(index)) {
+                return;
+            }
         }
         if (index == Player.Position) {
             return;
         }
         NightMapStateEnum target = m_map[index.x, index.y];
-        List<Vector2Int> paths = GoTo(index);
-        Debug.Log(paths.Count);
-        // Debug.Log(paths.Count);
-        return;
+        m_paths = GoTo(index);
         switch (target) {
             case NightMapStateEnum.CHEST: 
                 Debug.Log("OPEN CHEST"); 
@@ -114,8 +119,18 @@ public class NightManager : Frame.SceneManager {
                 break;
         }
     }
+    private void Move() {
+        Vector3 target = new Vector3(m_paths[0].y, -m_paths[0].x, Player.transform.position.z);
+        if (Vector3.Distance(Player.transform.position, target) <= 0.1f) {
+            Player.transform.localPosition = target;
+            Player.Position = m_paths[0];
+            ShowMap(Player.Position);
+            m_paths.RemoveAt(0);
+        } else {
+            Player.transform.Translate( m_playerSpeed * Time.deltaTime * (target - Player.transform.position));
+        }
+    }
     private List<Vector2Int> GoTo(Vector2Int target) {
-        Debug.Log("Begin to walk");
         int N = m_nightMapChanceSheet.MapCount;
         bool[] visited = new bool[N * N];
         void MarkVisited(Vector2Int pos) {
@@ -128,13 +143,17 @@ public class NightManager : Frame.SceneManager {
         List<Vector2Int> t = new();
         t.Add(Player.Position);
         path.Enqueue(t);
-        int times = 0;
+        int TIMES = 0;
         while (path.Count > 0) {
-            if (times++ >= 100) {
+            List<Vector2Int> currList = path.Dequeue();
+            if ( currList.Count >= 2 * N) {
+                // Debug.Log(string.Format("CAN NOT GO {0}", target))
+                continue;
+            }
+            if (TIMES++ >= N * N) {
                 // Debug.Log(string.Format("CAN NOT GO {0}", target))
                 return new List<Vector2Int>();
             }
-            List<Vector2Int> currList = path.Dequeue();
             Vector2Int current = currList[^1];
             if (current == target) {
                 return currList;    
@@ -142,27 +161,27 @@ public class NightManager : Frame.SceneManager {
             MarkVisited(current);
             Vector2Int left = new Vector2Int(current.x - 1, current.y);
             if (CanGo(left) && CheckVisited(left)) {
-                List<Vector2Int> to_add = new(currList);
-                to_add.Add(new Vector2Int(left.x, left.y));
-                path.Enqueue(to_add);
+                List<Vector2Int> toAdd = new(currList);
+                toAdd.Add(new Vector2Int(left.x, left.y));
+                path.Enqueue(toAdd);
             }
             Vector2Int right = new Vector2Int(current.x + 1, current.y);
             if (CanGo(right) && CheckVisited(right)) {
-                List<Vector2Int> to_add = new(currList);
-                to_add.Add(new Vector2Int(right.x, right.y));
-                path.Enqueue(to_add);
+                List<Vector2Int> toAdd = new(currList);
+                toAdd.Add(new Vector2Int(right.x, right.y));
+                path.Enqueue(toAdd);
             }
             Vector2Int up = new Vector2Int(current.x, current.y - 1);
             if (CanGo(up) && CheckVisited(up)) {
-                List<Vector2Int> to_add = new(currList);
-                to_add.Add(new Vector2Int(up.x, up.y));
-                path.Enqueue(to_add);
+                List<Vector2Int> toAdd = new(currList);
+                toAdd.Add(new Vector2Int(up.x, up.y));
+                path.Enqueue(toAdd);
             }
             Vector2Int down = new Vector2Int(current.x, current.y + 1);
             if (CanGo(down) && CheckVisited(down)) {
-                List<Vector2Int> to_add = new(currList);
-                to_add.Add(new Vector2Int(down.x, down.y));
-                path.Enqueue(to_add);
+                List<Vector2Int> toAdd = new(currList);
+                toAdd.Add(new Vector2Int(down.x, down.y));
+                path.Enqueue(toAdd);
             }
         }
         return new List<Vector2Int>();
@@ -170,7 +189,7 @@ public class NightManager : Frame.SceneManager {
     bool CanGo(Vector2Int pos) {
         int x = pos.x;
         int y = pos.y;
-        return InMap(x, y) && !m_shadow[x, y];   
+        return InMap(x, y) && CanGoAsShadow(pos);   
     }
     private int HandlePointToArrayIndex(float p) {
         return Mathf.RoundToInt(p);
@@ -178,9 +197,16 @@ public class NightManager : Frame.SceneManager {
     public bool InMap(int x, int y) {
         return x >= 0 && y >= 0  && x < m_nightMapChanceSheet.MapCount && y < m_nightMapChanceSheet.MapCount;
     }
+    private bool CheckShadow(int x, int y) {
+        return InMap(x, y) && m_shadow[x, y];
+    }
+    private bool CanGoAsShadow(Vector2Int index) {
+        return CheckShadow(index.x - 1, index.y) || CheckShadow(index.x + 1, index.y) ||
+            CheckShadow(index.x, index.y - 1) || CheckShadow(index.x, index.y + 1);
+    }
     public void ShowMap(Vector2Int pos) {
         ShowShadow(pos.x, pos.y);
-        if (InMap(pos.x - 1, pos.y)) {
+        /*if (InMap(pos.x - 1, pos.y)) {
             ShowShadow(pos.x - 1, pos.y);
         }
         if (InMap(pos.x + 1, pos.y)) {
@@ -203,11 +229,11 @@ public class NightManager : Frame.SceneManager {
         }
         if (InMap(pos.x + 1, pos.y + 1)) {
             ShowShadow(pos.x + 1, pos.y + 1);
-        }
+        }*/
     }
     private void ShowShadow(int r, int c) {
         m_shadow[r, c] = false;
-        Transform shadow = m_shadowTransform.GetChild(r * m_nightMapChanceSheet.MapCount + c);
+        Transform shadow = m_shadowParent.GetChild(r * m_nightMapChanceSheet.MapCount + c);
         shadow.gameObject.SetActive(false);
     }
 }
